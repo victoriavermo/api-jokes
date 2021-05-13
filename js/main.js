0f26647dfbfd530497791d79bcd967a69f68c9c2
victoriavermo <victoriavermo@gmail.com>
2021-05-13 14:47:06 +0200

    Ecercice 2 - Show the joke

--- js/main.js
+++ js/main.js
@@ -2,12 +2,19 @@ const url = 'https://icanhazdadjoke.com/';
 var header = { 
         headers: { Accept: "application/json" },
     };
+const jokeP = document.querySelector(".joke");    
+    
 
-//Function that get the joke from de API and show it through the console.
+//Function that get the joke from de API.
 function getData(){
 
     fetch(url,header)
         .then(response => response.json())
-        .then(data => console.log(data.joke));
-    
+        .then(data => showJoke(data.joke));
+}
+
+//Function that prints the joke.
+function showJoke(joke){
+    console.log(joke);
+    jokeP.innerText = joke;
 }
