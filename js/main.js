f4cd40746b0ca242a5c3b89fe6e19bae97606f31
victoriavermo <victoriavermo@gmail.com>
2021-05-13 19:53:10 +0200

    Logic Weather

--- js/main.js
+++ js/main.js
@@ -15,7 +15,7 @@ const tempP = document.querySelector(".temp");
 const minP  = document.querySelector(".min");
 const maxP = document.querySelector(".max");
 
-/* ---- EXERCICE 1: JOKE ---- */
+/* ---- EXERCICE 1 and 2: JOKE ---- */
 //Function that get the joke from de API.
 function getData(){
    fetch(url,header)
@@ -29,4 +29,46 @@ function showJoke(joke){
     jokeP.innerText = `" ${joke} "`;
 }
 
-/* ---- EXERCICE 2: WEATHER ---- */
+/* ---- EXERCICE 3: WEATHER ---- */
+window.onload = getWeather;
+
+function getWeather(){
+    fetch(urlWeahter)
+        .then(response => response.json())
+        .then(data => showWeather(data));
+}
+
+function showWeather(data){
+    let city = data.name;
+    let tiempo = chooseIcon(data.weather[0].main);
+    let description = data.weather[0].description;
+    let temp = data.main.temp;
+    let min = data.main.temp_min;
+    let max = data.main.temp_max;
+
+    cityP.innerText = city;
+    iconP.innerText = tiempo; 
+    descriptionP.innerText = description; 
+    tempP.innerText = temp+'ºC';
+    minP.innerText = min+'ºC';
+    maxP.innerText = max+'ºC';
+}
+
+function chooseIcon(weather){
+    switch(weather){
+        case 'Clouds':
+            return  '☁️';
+        case 'Atmosphere':
+            return '🌫️';
+        case 'Clear':
+            return '☀️'; 
+        case 'Snow':
+            return '❄️';  
+        case 'Rain':
+            return '🌧️';       
+        case 'Drizzle':
+            return '🌦️'; 
+        case 'Thunderstorm':
+            return '⛈️'; 
+    }
+}
