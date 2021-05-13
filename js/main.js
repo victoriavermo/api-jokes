5070f719a869c31ea3b22bd360750a3e701be0da
victoriavermo <victoriavermo@gmail.com>
2021-05-13 14:36:56 +0200

    Exercice 1

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@ var header = {
         headers: { Accept: "application/json" },
     };
 
+//Function that get the joke from de API and show it through the console.
 function getData(){
 
     fetch(url,header)
