681b60ae08a6965904bfa288841fbaded0c6184c
victoriavermo <victoriavermo@gmail.com>
2021-05-13 19:52:16 +0200

    Exercice 3 - API Open Weather Map

--- js/main.js
+++ js/main.js
@@ -1,13 +1,24 @@
+//Joke Variables
 const url = 'https://icanhazdadjoke.com/';
 const header = { 
         headers: { Accept: "application/json" },
     };
-const jokeP = document.querySelector(".joke");    
-    
+const jokeP = document.querySelector(".joke");
 
+//Weather Variables
+const key = config.API_KEY;
+const urlWeahter = `https://api.openweathermap.org/data/2.5/weather?q=Barcelona&units=metric&appid=${key}&lang=ca`;
+const cityP = document.querySelector(".city");
+const iconP = document.querySelector(".icon");
+const descriptionP = document.querySelector(".description");
+const tempP = document.querySelector(".temp");
+const minP  = document.querySelector(".min");
+const maxP = document.querySelector(".max");
+
+/* ---- EXERCICE 1: JOKE ---- */
 //Function that get the joke from de API.
 function getData(){
-    fetch(url,header)
+   fetch(url,header)
         .then(response => response.json())
         .then(data => showJoke(data.joke));
 }
@@ -15,6 +26,7 @@ function getData(){
 //Function that prints the joke.
 function showJoke(joke){
     console.log(joke);
-    jokeP.innerText = `" `+joke+` "`;
+    jokeP.innerText = `" ${joke} "`;
 }
 
+/* ---- EXERCICE 2: WEATHER ---- */
